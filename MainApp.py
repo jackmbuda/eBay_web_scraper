7f025e69bdf43feb839e5afcb846c6b2a2d6188f
jack <jackmbuda@gmail.com>
2020-03-27 14:08:19 -0500

    Removed unnecessary comments in MainApp.py

--- MainApp.py
+++ MainApp.py
@@ -2,8 +2,7 @@ from Qscraper import ScrapeHandler
 
 # This code was made for python 2 and will NOT work for python 3
 
-# my_Scraper object create from the ScrapeHandler class located in the Qscraper
-# file. Object initialized with the date in order to name the resulting csv file
+
 my_Scraper = ScrapeHandler()
 # my_Scraper calculatedPhonePrices and creates a csv file with appropriate
 # information. Csv file used as reference by a chat bot for buying used phones.
